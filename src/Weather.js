88fe3fbdc1aee67327ba8a53b3737fa87bf9bf78
Maxess <sharkkizayk@gmail.com>
2021-12-20 15:35:36 +0200

    fix-adaptive

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,13 @@
 import React, {useState} from 'react'
 import {useQuery} from '@apollo/client'
 import {WEATHER_LVIV} from "./graphQl/WeatherGraphQl";
-import {Box, Button, Drawer, Grid} from "@mui/material";
+import {Grid} from "@mui/material";
 import LeftBar from "./element/LeftBar/LeftBar";
 import Highlights from "./element/Highlights/Highlights";
 
 
 export const Weather = () => {
     const [city, setCity] = useState('London')
-    const [open, setOpen] = useState(false)
     const [isChecked, setCheck] = useState(true)
     const {data, loading, error} = useQuery(WEATHER_LVIV, {
         variables: {name: city}
@@ -27,7 +26,7 @@ export const Weather = () => {
     return <Grid container sx={{flexDirection: {xs: 'column', md: 'row' }, height:{md:'100vh'}}}>
         <Grid sx={{background: 'white', padding: 4, zIndex: 1000, boxShadow: 15}} item xs={12} md={3}>
                 <LeftBar
-                    time={data?.getCityByName?.weather.timestamp}
+                    time={data?.getCityByName?.weather?.timestamp}
                     loading={loading}
                     dataWeather={data?.getCityByName}
                     cityName={city}
